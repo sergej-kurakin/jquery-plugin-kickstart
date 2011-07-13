71a10394b1176e2c0242fef8ec22178bc553aeeb
Sergej Kurakin <sergej@kurakin.info>
2011-07-13 23:05:11 +0300

    Removing methods that does nothing

--- tests/class-extender.qunit/classes-objects.js
+++ tests/class-extender.qunit/classes-objects.js
@@ -7,17 +7,8 @@ var myClass1 = function  (name) {
 	this.init(name);
 }
 
-myClass1.prototype.test = function () {
-	console.log('myClass1::test:  ' + this.text);
-};
-
 myClass1.prototype.init = function (name) {
 	this.text = name;
-};
-
-myClass1.prototype.test2 = function () {
-	console.log('myClass1::test2: ' + this.text);
-	console.log('myClass1::test2: ' + this.className);
 };
 
 
@@ -32,16 +23,6 @@ var myClass2 = function (name) {
 
 $.extendClass(myClass2, myClass1);
 
-myClass2.prototype.test = function () {
-	console.log('myClass2::test');
-	myClass2.parent.test.apply(this);
-	console.log('myClass2::test');
-};
-
 var o1 = new myClass1('One');
-o1.test();
-o1.test2();
 
 var o2 = new myClass2('Two');
-o2.test();
-o2.test2();
